38700937a636c24704a01714bb1f2fe875e992dd
Victoria Curl <victoriacurl9@gmail.com>
2019-12-21 19:19:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,21 @@
-def reverse_each_word_with_each(string)
+#def reverse_each_word_with_each(string)
 
-  array = string.split(" ")
-  my_array = []
-  string.each do |string|
-    my_array << string.reverse
-  end
-  my_array.join("")
-end
+  #array = string.split(" ")
+  #my_array = []
+  #string.each do |string|
+    #my_array << string.reverse
+  #end
+  #my_array.join("")
+#end
 
-def reverse_each_word(string)
+#def reverse_each_word(string)
 
-  another_array = string.split(" ")
-  test_array = []
-  another_array.collect do |string|
-    test_array << string.reverse
-  end
-  test_array.join("")
-end
+  #another_array = string.split(" ")
+  #test_array = []
+  #another_array.collect do |string|
+  #  test_array << string.reverse
+  #end
+  #test_array.join("")
+#end
 
-reverse_each_word("Hello, there and who are you?")
+#reverse_each_word("Hello, there and who are you?")
