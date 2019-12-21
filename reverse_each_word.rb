a0327b6b0a18a16daa1be8c627f3bdc0ecd0fd41
Victoria Curl <victoriacurl9@gmail.com>
2019-12-21 19:02:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(string)
+def reverse_each_word_with_each(string)
 
   array = string.split("")
   my_array = []
@@ -7,3 +7,6 @@ def reverse_each_word(string)
   end
   my_array.join("")
 end
+
+def reverse_each_word(string)
+  
